8ef8601307f6e90ca4e709712c80f914e59820b4
FranG-A <frangreen-armytage@student-078.some.ox.ac.uk>
2017-03-05 14:35:41 +0000

    bunchofstuff

--- outofhalls.py
+++ outofhalls.py
@@ -15,10 +15,6 @@ Cowley = ["Queens", "Teddy_Hall", "University", "Merton", "Magdalen", "St_Hildas
 Cornmarket = ["Oriel", "Corpus_Christi", "Christ_Church", "Pembroke", "Nuffield", "St_Peters"]
 
 
-#@app.route("/index", methods)
-#def normal():
-#    return render_template("outofhalls.html", )
-
 @app.route("/")
 def outofhalls():
     return render_template("indexbod.html")
@@ -46,5 +42,27 @@ def cowley():
 @app.route("/cornmarket")
 def cornmarket():
     return render_template("cornmarket.html")
+
+
+def send_simple_message(var):
+    print "sending email to {0}".format(var)
+    return requests.post(
+        "https://api.mailgun.net/v3/sandbox62c1649bfb464d9781537ad48f67a7dd.mailgun.org/messages",
+        auth=("api", "key-b6e18fc0572d4d9c98c99abe2b4d2850"),
+        data={"from": "Excited User <mailgun@sandbox62c1649bfb464d9781537ad48f67a7dd.mailgun.org>",
+              "to": var,
+              "subject": "Hello",
+              "text": "Thank you for your email! We have taken your response and will review it shortly! Check back to see if your idea makes it onto the website!"})
+
+@app.route("/signup", methods=['POST'])
+def sign_up():
+    form_data = request.form
+    print form_data['name']
+    email_address = form_data['email']
+    print email_address
+    print form_data['fave_spot']
+    send_simple_message(email_address)
+    return "All Ok."
+
 
 app.run(debug = True)
